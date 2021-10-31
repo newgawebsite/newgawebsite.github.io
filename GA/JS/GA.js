034c95e2f68af15ed6c830426e2d7dd4c63c3e62
joshbunyan <78365838+joshbunyan@users.noreply.github.com>
2021-10-31 14:29:24 -0500

    Update GA.js

--- GA/JS/GA.js
+++ GA/JS/GA.js
@@ -35,15 +35,15 @@ var msa = window.matchMedia("(min-height: 1px) and (max-height: 1100px)");
 
 function missionStatement() {
     if (msa.matches) {
-        if (document.documentElement.scrollTop > 250) {
-            document.getElementById("MS").className = "mission-statement col-10 animate__animated animate__fadeInRight";
+        if (document.documentElement.scrollTop > 275) {
+            document.getElementById("MS").className = "mission-statement col-9 animate__animated animate__fadeInRight";
         }
     }
 }
 
 function ministries() {
     if (msa.matches) {
-        if (document.documentElement.scrollTop > 730) {
+        if (document.documentElement.scrollTop > 760) {
             document.getElementById("Ministries").className = "ministries-title col-12 animate__animated animate__zoomIn";
         }
     }
