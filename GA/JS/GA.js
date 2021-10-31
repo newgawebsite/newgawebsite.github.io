9df95d1acf481e6ea031c3f5a770601b69c7b967
joshbunyan <78365838+joshbunyan@users.noreply.github.com>
2021-10-31 13:37:28 -0500

    Update GA.js

--- GA/JS/GA.js
+++ GA/JS/GA.js
@@ -34,7 +34,7 @@ var msa = window.matchMedia("(min-height: 1px) and (max-height: 800px)");
 
 function missionStatement() {
     if (msa.matches) {
-        if (document.documentElement.scrollTop > 500) {
+        if (document.documentElement.scrollTop > 650) {
             document.getElementById("MS").className = "mission-statement-img col-6 animate__animated animate__fadeInRight";
         }
     }
