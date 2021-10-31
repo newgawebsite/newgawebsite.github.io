18276e152a6e9ff95881d58979fdb68925bd1fb2
joshbunyan <78365838+joshbunyan@users.noreply.github.com>
2021-10-31 14:26:40 -0500

    Update GA.js

--- GA/JS/GA.js
+++ GA/JS/GA.js
@@ -43,7 +43,7 @@ function missionStatement() {
 
 function ministries() {
     if (msa.matches) {
-        if (document.documentElement.scrollTop > 650) {
+        if (document.documentElement.scrollTop > 730) {
             document.getElementById("Ministries").className = "ministries-title col-12 animate__animated animate__zoomIn";
         }
     }
