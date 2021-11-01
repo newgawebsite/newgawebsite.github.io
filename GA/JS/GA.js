fd4b61dc2211600ad3ec5abda50ba377943a611e
joshbunyan <78365838+joshbunyan@users.noreply.github.com>
2021-11-01 11:07:35 -0500

    Moved everything up

--- GA/JS/GA.js
+++ GA/JS/GA.js
@@ -35,7 +35,7 @@ var msa = window.matchMedia("(min-height: 1px) and (max-height: 1100px)");
 
 function missionStatement() {
     if (msa.matches) {
-        if (document.documentElement.scrollTop > 230) {
+        if (document.documentElement.scrollTop > 215) {
             document.getElementById("MS").className = "mission-statement col-9 animate__animated animate__fadeInRight";
         }
     }
@@ -43,7 +43,7 @@ function missionStatement() {
 
 function ministries() {
     if (msa.matches) {
-        if (document.documentElement.scrollTop > 720) {
+        if (document.documentElement.scrollTop > 700) {
             document.getElementById("Ministries").className = "ministries-title col-12 animate__animated animate__zoomIn";
         }
     }
