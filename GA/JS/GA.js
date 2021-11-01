8e06ea6c24ba8599c345e5208455a98b2fb29922
joshbunyan <78365838+joshbunyan@users.noreply.github.com>
2021-11-01 11:05:14 -0500

    Made OnScroll, (JS), happen sooner

--- GA/JS/GA.js
+++ GA/JS/GA.js
@@ -35,7 +35,7 @@ var msa = window.matchMedia("(min-height: 1px) and (max-height: 1100px)");
 
 function missionStatement() {
     if (msa.matches) {
-        if (document.documentElement.scrollTop > 250) {
+        if (document.documentElement.scrollTop > 230) {
             document.getElementById("MS").className = "mission-statement col-9 animate__animated animate__fadeInRight";
         }
     }
@@ -43,7 +43,7 @@ function missionStatement() {
 
 function ministries() {
     if (msa.matches) {
-        if (document.documentElement.scrollTop > 750) {
+        if (document.documentElement.scrollTop > 720) {
             document.getElementById("Ministries").className = "ministries-title col-12 animate__animated animate__zoomIn";
         }
     }
